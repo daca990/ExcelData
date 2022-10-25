9fdf1323578a38958d64487e555dd8823f1de754
Danica <daca.ciric990@gmail.com>
2022-10-26 00:12:55 +0200

    Commit changes from American guy

---
+++ src/test/java/RestAPITest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+  @Test
+    public void postJIra(){
+      System.out.println("post Jira");
+      System.out.println("post Jira2");
+      System.out.println("post Jira3");
+  }
+}
